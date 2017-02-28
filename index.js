63477b85c0f561d6891ecae83899784489363062
Timothy Tavarez <tavarez.timothy.m@gmail.com>
2017-02-28 16:15:36 -0700

    Add navigation section. Document still unstructured.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 let titleText = 'Best Page on WWW'
 
-function updateTitle(text) {
+function updateDocumentTitle(text) {
   let title = document.createElement('title'),
       titleNodeText = document.createTextNode(text);
       title.appendChild(titleNodeText);
@@ -12,19 +12,36 @@ function updateTitle(text) {
   return document.head.appendChild(title)
 }
 
-updateTitle(titleText);
-
-// header
-
-
+updateDocumentTitle(titleText);
 
 // body
 
-let heading = document.createElement("h1"),
-    headingText = document.createTextNode("DOM manipulation via JavaScript is cool.");
+  // navigation section
 
-heading.appendChild(headingText);
-document.body.appendChild(heading);
+  let navInsert = document.createElement('nav');
+  document.body.appendChild(navInsert);
 
-let headingEl = getElementsByTagName('h1');
-headingEl.style.color = 'red';
+  let nav = document.querySelector('nav').className = "nav";
+
+  let ulInsert = document.createElement('ul');
+  document.body.appendChild(ulInsert);
+
+  function createListItems(items) {
+    for (let i = 0; i <= items; i++ ) {
+      let li = document.createElement('li'),
+          liContent = document.createTextNode(i.toString());
+      li.appendChild(liContent);
+
+      document.body.appendChild(li);
+    }
+  }
+
+  createListItems(4);
+
+  // add some spice to the page.
+
+  let heading = document.createElement('h1'),
+      headingText = document.createTextNode('DOM manipulation via JavaScript is cool.');
+
+  heading.appendChild(headingText);
+  document.body.appendChild(heading);
